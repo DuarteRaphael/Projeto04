b2fe57dd433b3bc842c37c904d29e17e3ebc9f5e
DuarteRaphael <raphael-duarte@outlook.com>
2023-06-10 17:45:51 -0300

    ...
    ...

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("\n - CONTROLE DE PRODUTOS E FORNECEDORES - \n");
+
+try
+{
+    //definindo o caminho do banco de dados
+    var connectionstring = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BDProjeto04;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    
+    //iniciando o cadastro de um fornecedor
+    Console.WriteLine("\nCADASTRO DE FORNECEDOR\n");
+}
+catch (Exception e)
+{
+    Console.WriteLine($"\nErro: {e.Message}");
+}
+
+Console.ReadKey();
--- Repositories/FornecedorRepository.cs
+++ Repositories/FornecedorRepository.cs
@@ -1,13 +1,61 @@
-﻿using System;
+﻿using Dapper;
+using Projeto04.Entities;
+using Projeto04.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Projeto04.Repositories
 {
-    public class FornecedorRepository
+    /// <summary>
+    /// Classe para repositório de banco de dados de fornecedor.
+    /// </summary>
+    public class FornecedorRepository : IFornecedorRepository
     {
+        //atributo privado
+        private readonly string _connectionstring;
 
+        //método construtor da classe utilizado para que possamos passar como parametro o valor da connectionstring do banco de dados para a classe FornecedorRepository
+        public FornecedorRepository(string connectionstring)
+        {
+            _connectionstring = connectionstring;
+        }
+
+        public void Inserir(Fornecedor obj)
+        {
+            //escrevendo o comando SQL que será executado no banco.
+            var query = @"INSERT INTO FORNECEDOR(IDFORNECEDOR, NOME, CNPJ) VALUES(@IdFornecedor, @Nome, @Cnpj)";
+
+            //abrindo uma conexão com o banco de dados
+            using (var connection = new SqlConnection(_connectionstring))
+            {
+                //executando a query no banco de dados
+                //passando os dados do objeto fornecedor
+                connection.Execute(query, obj);
+            }
+        }
+
+        public void Alterar(Fornecedor obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(Fornecedor obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Fornecedor> Consultar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Fornecedor ConsultarPorCnpj(string cnpj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
